b7caa4467a327d47959cd15c43672c7c18792ce6
Jack Zhuang <50353452+hotlong@users.noreply.github.com>
2026-01-27 08:32:45 +0800

    feat: Remove Datasource and Manifest schemas from the system module

--- packages/spec/src/system/index.ts
+++ packages/spec/src/system/index.ts
@@ -8,11 +8,13 @@
  */
 
 export * from './audit.zod';
-export * from './manifest.zod';
-export * from './datasource.zod';
 export * from './translation.zod';
 export * from './events.zod';
 export * from './job.zod';
 export * from './types';
+
+// Re-export Core System Definitions from other modules to form the "ObjectOS" namespace
+export * from '../kernel/manifest.zod';
+export * from '../driver/datasource.zod';
 // Note: Auth, Identity, Policy, Role, Organization moved to @objectstack/spec/auth
 // Note: Territory moved to @objectstack/spec/permission
